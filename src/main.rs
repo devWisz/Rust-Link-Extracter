d133619cb0a17b3ecbadf59f1379377431a915dd
Sarjak Khanal <pythondevforuser@gmail.com>
2026-05-19 20:09:31 +0000

    after hours of debugging it is working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
-use error_chain ::error_chain;
-use select :: document ::Document;
-use select :: predicate ::Name;
+use select::document::Document;
+use select::predicate::Name;
+use reqwest::header::USER_AGENT;
 
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let target_url = "https://www.rust-lang.org/en-US/";
+    println!("Fetching {}...", target_url);
 
-error_chain {
-    foreign_links {
-   ReqError(request::Error);
-   IoError(std ::io::Error);
-    }
+    let client = reqwest::Client::new();
+    let res = client.get(target_url)
+        .header(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("Links found:\n");
+
+    Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|link| {
+            if link.starts_with('/') {
+              
+                println!("https://www.rust-lang.org{}", link);
+            } else if link.starts_with("http") {
+             
+                println!("{}", link);
+            }
+        });
+
+    Ok(())
 }
